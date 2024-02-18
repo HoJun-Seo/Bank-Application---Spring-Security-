d2291ccdf93d4b77656161ce837491ab2ff14fcf
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2024-02-18 19:56:35 +0900

    Bank Application

--- bank/src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
+++ bank/src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import shop.mtcoding.bank.domain.user.User;
@@ -15,13 +16,21 @@ public class UserReqDto {
     @Setter
     public static class JoinReqDto {
 
-        @Pattern(regexp = "", message = "영문/숫자 2~20자 이내로 작성해주세요")
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요")
         @NotEmpty // null 이거나 공백일 수 없다.
         private String username;
+
+        // 패스워드는 최소, 최대 길이만 지정한다
+        // @Size 어노테이션은 String 타입의 데이터에만 사용가능하다.
+        @Size(min = 4, max = 20)
         @NotEmpty
         private String password;
+
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", message = "이메일 형식으로 작성해주세요")
         @NotEmpty
         private String email;
+
+        @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "영문/한글 1~20자 이내로 작성해주세요")
         @NotEmpty
         private String fullname;
 
--- bank/src/test/java/shop/mtcoding/bank/temp/RegexTest.java
+++ bank/src/test/java/shop/mtcoding/bank/temp/RegexTest.java
@@ -55,4 +55,33 @@ public class RegexTest {
         boolean result = Pattern.matches("^[a-zA-Z]{2,4}$", value);
         System.out.println("테스트 : " + result);
     }
+
+    @Test
+    public void user_usernameTest() throws Exception {
+        // UserReqDto 에서 @Pattern 어노테이션으로 정해둔 방식으로 문자열을 매칭해줄 정규표현식 작성
+        // 영문, 숫자 2 ~ 20 자 이내로 작성
+        // 영어, 숫자외에 다른 문자가 들어오거나 길이 조건을 충족하지 않으면 false 를 반환한다.
+        String username = "ssar";
+        boolean result = Pattern.matches("^[a-zA-Z0-9]{2,20}$", username);
+        System.out.println("테스트 : " + result);
+    }
+
+    @Test
+    public void user_fullnameTest() throws Exception {
+        // 영어, 또는 한글 1 ~ 20자 까지 허용된다. ㄱ,ㄴ 같은 단일 글자는 허용되지 않는다.
+        String fullname = "쌀";
+        boolean result = Pattern.matches("^[a-zA-Z가-힣]{1,20}$", fullname);
+        System.out.println("테스트 : " + result);
+    }
+
+    @Test
+    public void user_emailTest() throws Exception {
+
+        // 이메일 형식 매칭을 제대로 해주려면 ac.kr, co.kr, or.kr 등 다양한 이메일 형식을 모두 잡아주는 등
+        // 보다 자세하게 조건을 작성해놓은 정규표현식이 필요하다.
+        // 추후에 알아보거나 이메일 형식에 대한 정규표현식을 지원해주는 라이브러리를 사용하면 된다.
+        String fullname = "ssar@nate.com";
+        boolean result = Pattern.matches("^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", fullname);
+        System.out.println("테스트 : " + result);
+    }
 }
