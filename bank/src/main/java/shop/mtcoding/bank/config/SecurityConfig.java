932232d30110dde3357a9c0baffe5c81b7bc64e0
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2024-02-22 21:50:01 +0900

    Bank Application

--- bank/src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
+++ bank/src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
@@ -5,9 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -30,11 +35,21 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    public class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
+
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            builder.addFilter(new JwtAuthenticationFilter(authenticationManager));
+            super.configure(builder);
+        }
+    }
+
     @Bean
     // JWT 서버를 만들 예정이기 때문에 세션은 사용하지 않는다.(시큐리티 세션 사용)
     public <T> SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         log.debug("디버그 : filterChain 빈 등록됨");
-        // iframe 허용하지 않음(HTML 의 iframe을 허용하지 않는다. iframe 검색해볼것)
+
         http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         http.csrf(csrf -> csrf.disable()); // enable 이면 포스트맨이 동작하지 못함
         http.cors(cors -> cors.configurationSource(configurationSource())); // cors 를 승인해두지 않으면 컨트롤러에 자바스크립트로 전송되는 요청을
@@ -42,17 +57,12 @@ public class SecurityConfig {
 
         // 세션 id 를 서버쪽에서 관리하지 않겠다는 뜻
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        // 리액트, 앱에서 들어오는 요청을 받을 것이기 때문에 form 로그인을 사용하지 않는다.
         http.formLogin(form -> form.disable());
-        // httpBasic 은 브라우저가 팝업창을 이용해서 사용자 인증을 진행한다.
-        // 이를 승인거부 상태로 해두지 않으면 인증이 필요한 페이지로 이동할 때
-        // 갑자기 브라우저에서 인증을 받기위한 팝업창을 띄우게된다.
         http.httpBasic(httpBasic -> httpBasic.disable());
 
-        // JWT 필터 적용
-        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
-        http.addFilterBefore(new JwtAuthenticationFilter(authenticationManager),
-                UsernamePasswordAuthenticationFilter.class);
+        // JWT 필터 등록
+        http.apply(new CustomSecurityFilterManager());
+
         // Exception 가로채기
         http.exceptionHandling(handle -> handle.authenticationEntryPoint((request, response, authException) -> {
             CustomResponseUtil.unAuthentication(response, "로그인을 진행해주세요");
