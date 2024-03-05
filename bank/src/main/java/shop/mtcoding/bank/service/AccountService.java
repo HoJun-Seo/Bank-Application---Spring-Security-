c2b12eef59c2511d8c4bc4caac563cb6a725ce3a
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2024-03-06 00:30:58 +0900

    Bank Application

--- bank/src/main/java/shop/mtcoding/bank/service/AccountService.java
+++ bank/src/main/java/shop/mtcoding/bank/service/AccountService.java
@@ -1,5 +1,7 @@
 package shop.mtcoding.bank.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,6 +10,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
@@ -26,17 +29,38 @@ public class AccountService {
     // User 로그인되어 있는 상태여야함
     // 로그인 되어있는지에 대한 확인은 Service 가 할일이 아님(컨트롤러가 할 일)
     @Transactional
-    public void accountRegister(AccountSaveReqDto accountSaveReqDto, Long userId) {
+    public AccountSaveRespDto accountRegister(AccountSaveReqDto accountSaveReqDto, Long userId) {
         // User 가 DB에 있는지 검증을 위한 엔티티 필요
         User userPS = userRepository.findById(userId).orElseThrow(
                 () -> new CustomApiException("유저를 찾을 수 없습니다."));
 
         // 해당 계좌가 DB에 있는지 중복여부 체크
-        accountRepository.findByNumber(accountSaveReqDto.getNumber());
+        Optional<Account> accountOP = accountRepository.findByNumber(accountSaveReqDto.getNumber());
+        if (accountOP.isPresent()) {
+            throw new CustomApiException("해당 계좌가 이미 존재합니다.");
+        }
 
         // 계좌 등록
+        Account accountPS = accountRepository.save(accountSaveReqDto.toEntity(userPS));
 
         // DTO를 응답
+        return new AccountSaveRespDto(accountPS);
+    }
+
+    // 응답을 위한 DTO 생성
+    @Getter
+    @Setter
+    public static class AccountSaveRespDto {
+        private Long id;
+        private Long number;
+        private Long balance;
+
+        public AccountSaveRespDto(Account account) {
+            this.id = account.getId();
+            this.number = account.getNumber();
+            this.balance = account.getBalance();
+        }
+
     }
 
     // 계좌등록 요청 시 활용할 DTO
@@ -55,5 +79,14 @@ public class AccountService {
 
         // 잔액(초기 잔액 1000원)과 user 정보는 받을 필요없음
         // user 정보는 세션에서 가져오면 됨
+
+        public Account toEntity(User user) {
+            return Account.builder()
+                    .number(number)
+                    .password(password)
+                    .balance(1000L)
+                    .user(user)
+                    .build();
+        }
     }
 }
