c6d4711a62a15c0be7bd02b591a16270f48e5779
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2024-03-20 18:24:20 +0900

    Bank Application

--- bank/src/main/java/shop/mtcoding/bank/config/dummy/DummyObject.java
+++ bank/src/main/java/shop/mtcoding/bank/config/dummy/DummyObject.java
@@ -67,40 +67,23 @@ public class DummyObject {
                 .build();
     }
 
-    protected Transaction newTransaction(Account withdrawAccount, Account depositAccount, Long amount,
-            Long withdrawAccountBalance, Long depositAccountBalance, TransactionEnum gubun, String sender,
-            String receiver, String tel) {
+    // 계좌 잔액이 1100원이 되고 나서 입금 트랜잭션 히스토리가 생성되어야 함
+    protected Transaction newMockDepositTransaction(Long id, Account account) {
 
-        return Transaction.builder()
-                .withdrawAccount(withdrawAccount)
-                .depositAccount(depositAccount)
-                .amount(amount)
-                .withdrawAccountBalance(withdrawAccountBalance)
-                .depositAccountBalance(depositAccountBalance)
-                .gubun(gubun)
-                .sender(sender)
-                .receiver(receiver)
-                .tel(tel)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-    }
-
-    protected Transaction newMockTransaction(Long id, Account withdrawAccount, Account depositAccount, Long amount,
-            Long withdrawAccountBalance, Long depositAccountBalance, TransactionEnum gubun, String sender,
-            String receiver, String tel) {
-
+        account.deposit(100L); // 거래내역에서 미리 매개변수로 넘어온 account 객체의 잔액을 늘려놓기
+        // 테스트 때 accountDeposit 메서드의 반환값으로 돌아온 AccountDepositRespDto 객체가 가지고있는 잔액 값을
+        // 검증해주기 위함
         return Transaction.builder()
                 .id(id)
-                .withdrawAccount(withdrawAccount)
-                .depositAccount(depositAccount)
-                .amount(amount)
-                .withdrawAccountBalance(withdrawAccountBalance)
-                .depositAccountBalance(depositAccountBalance)
-                .gubun(gubun)
-                .sender(sender)
-                .receiver(receiver)
-                .tel(tel)
+                .withdrawAccount(null) // 출금기능이 아니기에 출금 계좌는 null 값 고정
+                .depositAccount(account)
+                .amount(100L) // 입금금액 100원 고정, 굳이 매개변수를 통해 입금하고자 하는 값을 받지않음
+                .withdrawAccountBalance(null) // 출금이후 계좌잔액값 null
+                .depositAccountBalance(account.getBalance())
+                .gubun(TransactionEnum.DEPOSIT)
+                .sender("ATM")
+                .receiver(account.getNumber() + "")
+                .tel("01022227777") // 입금자 전화번호값 고정
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
                 .build();
